4a05a7eed20b2f56a3646173ec9edb3000b26641
vinay <vinaygangishetty96@gmail.com>
2020-06-09 14:23:03 +0530

    [vinay]Added:Added Swagger and Model Mapper configurations

---
+++ src/main/java/com/bridgelabz/parkinglotbackendapi/configuration/AppConfiguration.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.parkinglotbackendapi.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+}
